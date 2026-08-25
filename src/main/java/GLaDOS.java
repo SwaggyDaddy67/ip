06bc01045695386e0401d3365087ce0aa6c8d8c4
Bhao Tanush <224396818+SwaggyDaddy67@users.noreply.github.com>
2026-08-25 23:03:08 +0800

    Add support for echoing user commands

--- src/main/java/GLaDOS.java
+++ src/main/java/GLaDOS.java
@@ -1,21 +1,36 @@
-// Controls the banner and Entry/Exit message of ChatBot
+import java.util.Scanner;
+
+// Controls the banner, entry/exit messages, and command loop of ChatBot
 
 public class GLaDOS {
     public static void main(String[] args) {
-        String banner = "   ________          ____  ____  _____\n"
-                + "  / ____/ /   ____ _/ __ \\/ __ \\/ ___/\n"
-                + " / / __/ /   / __ `/ / / / / / /\\__ \\ \n"
-                + "/ /_/ / /___/ /_/ / /_/ / /_/ /___/ / \n"
-                + "\\____/_____/\\__,_/_____/\\____//____/  \n";
-        String line = "____________________________________________________________";
+        String banner = "        ________          ____  ____  _____\n"
+                + "       / ____/ /   ____ _/ __ \\/ __ \\/ ___/\n"
+                + "      / / __/ /   / __ `/ / / / / / /\\__ \\ \n"
+                + "     / /_/ / /___/ /_/ / /_/ / /_/ /___/ / \n"
+                + "     \\____/_____/\\__,_/_____/\\____//____/  \n";
+        String indent = "     ";
+        String line = "    ____________________________________________________________";
 
         System.out.println(line);
         System.out.println(banner);
-        System.out.println("Hello, I'm GLaDOS nice to... Oh, it's you.");
-        System.out.println("State your query. I have other tests to run.");
+        System.out.println(indent + "Hello, I'm GLaDOS nice to... Oh, it's you.");
+        System.out.println(indent + "State your query. I have other tests to run.");
+        System.out.println(line);
+
+        // Echo each command back until the user types "bye".
+        Scanner in = new Scanner(System.in);
+        String input = in.nextLine();
+
+        while (!input.equals("bye")) {
+            System.out.println(line);
+            System.out.println(indent + input);
+            System.out.println(line);
+            input = in.nextLine();
+        }
 
         System.out.println(line);
-        System.out.println("Test concluded. Try not to disappoint me next time.");
+        System.out.println(indent + "Test concluded. Try not to disappoint me next time.");
         System.out.println(line);
     }
 }
